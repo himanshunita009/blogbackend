0aba5294f434b038a48ed6b3a8d11ba69ae410b3
Himanshu Seth <97351955+himanshunita009@users.noreply.github.com>
2023-04-03 22:48:42 +0530

    final changes done

--- app.js
+++ app.js
@@ -31,10 +31,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
 /*Mongoose Database */
 
 
