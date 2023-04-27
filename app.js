38e8b0c58240f6ba5a8e3172e637348ce6adb6bc
Himanshu Seth <97351955+himanshunita009@users.noreply.github.com>
2023-04-27 11:14:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: "https://blogwala.onrender.com",
+  origin: "http://localhost:3000",
   credentials: true
 }));
 /*Mongoose Database */
